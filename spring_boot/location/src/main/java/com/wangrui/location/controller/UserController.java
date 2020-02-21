61cbb53a4371ee5a3a841452e0b49020a20a686f
sunjava2006 <jkwangrui@126.com>
2020-02-21 10:19:00 +0800

    'auto_login'

--- spring_boot/location/src/main/java/com/wangrui/location/controller/UserController.java
+++ spring_boot/location/src/main/java/com/wangrui/location/controller/UserController.java
@@ -6,8 +6,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -61,6 +65,41 @@ public class UserController {
 			mv.addObject("msg", "用户名和密码都是admin。");
 		}
 		return mv;
+	}
+	
+	@GetMapping("/autoLogin")// @RequestMapping(path="/autoLogin",method = RequestMethod.GET)
+	public String autoLogin(@CookieValue("loginName")  String loginName,
+			                @CookieValue("pwd") String passwd,
+			                Model m,
+			                HttpSession session) {
+		String viewName = "redirect:/index.html";
+		
+		if("admin".equals(loginName) && "admin".equals(passwd)) {
+			session.setAttribute("userInfo", loginName);
+			
+		}else {
+			viewName = "redirect:/loginpage";
+			
+		}
+		
+		return viewName;
+	}
+	
+	@RequestMapping("logout")
+	public String logout(HttpSession session, HttpServletResponse response) {
+		session.removeAttribute("userInfo"); // 删除Session中的用户信息
+		session.invalidate(); // 直接让Session失效。删除了Session所有的数据。
+		
+		//覆盖自动登录的Cookie数据
+		Cookie c1 = new Cookie("loginName", null);
+		c1.setMaxAge(0); // 让loginName这个Cookie失效
+		Cookie c2 = new Cookie("pwd", null);
+		c2.setMaxAge(0); // 让pwd这个Cookie失效
+		
+		response.addCookie(c1);
+		response.addCookie(c2);
+		
+		return "redirect:/";
 	}
 	
 }
--- spring_boot/location/src/main/java/com/wangrui/location/interseptor/LoginInterseptor.java
+++ spring_boot/location/src/main/java/com/wangrui/location/interseptor/LoginInterseptor.java
@@ -1,5 +1,6 @@
 package com.wangrui.location.interseptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,10 +19,32 @@ public class LoginInterseptor implements HandlerInterceptor {
 		}
 		String url = request.getRequestURI();
 		System.out.println("url:" + url);
+		
+		// 从Session中取出数据。
 		Object o = request.getSession(true).getAttribute("userInfo");
-		if("/".equals(url) || "/adminLogin".equals(url)|| (o!=null) ){
+		if("/autoLogin".equals(url)|| "/login".equals(url) || "/".equals(url) || "/adminLogin".equals(url)|| (o!=null) ){
 			return true;
 		}
+		if(null == o) {// 在没有登录的情况下，查看是否有可自动登录的Cookie
+			Cookie[] cookies = request.getCookies();
+			boolean loginName = false;
+			boolean pwd = false;
+			for(Cookie  c : cookies) {
+				String name = c.getName();
+				String val = c.getValue();
+				if("loginName".equals(name) && val!=null) {
+					loginName = true;
+				}
+				if("pwd".equals(name) && val!=null) {
+					pwd = true;
+				}
+			}
+			if(loginName && pwd) {
+				response.sendRedirect("/autoLogin");//响应对象向浏览器发送一个重定向指令。
+			}
+		}
+		
+		
 		
 		
 		return false;
